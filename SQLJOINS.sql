a93e6887b3b9d44edd6bd5c2e59361a89b8c6a43
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-03-12 21:32:29 +0530

    practice queries

---
+++ SQLJOINS.sql
@@ -0,0 +1,110 @@
+
+--join 
+use customerDb
+
+create schema hr;
+
+GO
+create table hr.camdidates(
+id int Primary key Identity,
+fullname varchar(100) NOT NULL
+);
+
+CREATE TABLE hr.employees(
+id int primary key identity,
+fullname varchar(100) NOT NULL
+);
+
+insert into hr.camdidates(fullname) values('asmita girhepunje'),('Lily bush'),('aniket dangre'),('pranay,dangre');
+
+insert into hr.employees(fullname) values('asmita girhepunje'),('Liga bush'),('rajani nhoyar'),('pranit yerwade');
+
+--inner join
+
+select c.id candidate_id,
+c.fullname candidate_name,
+e.id employee_id,
+e.fullname employee_name
+from hr.camdidates c
+INNER JOIN hr.employees e on c.fullname = e.fullname;
+
+-- left join 
+
+select c.id candidate_id,
+c.fullname candidate_name,
+e.id employee_id,
+e.fullname employee_name
+from hr.camdidates c
+LEFT JOIN hr.employees e on c.fullname = e.fullname;
+
+Select c.id candidate_id,
+c.fullname candidate_name,
+e.id employee_id,
+e.fullname employee_name
+from hr.camdidates c
+LEFT JOIN hr.employees e on c.fullname = e.fullname
+where e.id IS NULL;
+
+--right join 
+select c.id candidate_id,
+c.fullname candidate_name,
+e.id employee_id,
+e.fullname employee_name
+from hr.camdidates c
+Right JOIN hr.employees e on c.fullname = e.fullname
+where c.id Is NULL
+
+--FULL JOIN
+Select c.id candidate_id,
+c.fullname candidate_name,
+e.id employee_id,
+e.fullname employee_name
+from hr.camdidates c
+FULL JOIN hr.employees e on c.fullname = e.fullname;
+
+Select c.id candidate_id,
+c.fullname candidate_name,
+e.id employee_id,
+e.fullname employee_name
+from hr.camdidates c
+FULL JOIN hr.employees e on c.fullname = e.fullname
+where c.id IS NULL OR e.id IS NULL
+
+SELECT first_name, last_name from sales.customers where state = 'CA'group by first_name, last_name;
+
+--GROPU BY
+select Customer_id , YEAR (order_date) order_year from sales.orders where customer_id IN(1,2) group by customer_id, YEAR(order_date) order by customer_id;
+
+--GROPU BY
+select Distinct Customer_id , YEAR (order_date) order_year from sales.orders where customer_id IN(1,2) group by customer_id, YEAR(order_date) order by customer_id;
+
+select Customer_id , YEAR (order_date) order_year, COUNT(order_id) order_palced from sales.orders where customer_id IN(1,2) group by customer_id, YEAR(order_date) order by customer_id;
+
+select brand_name, MIN(list_price) min_price, MAX(list_price) from production.products p
+INNER JOIN production.brands b on b.brand_id = p.brand_id
+where model_year = 2018
+group by brand_name
+order by brand_name;
+
+select brand_name, AVG(list_price) AVG_price from production.products p
+INNER JOIN production.brands b on b.brand_id = p.brand_id
+where model_year = 2018
+group by brand_name
+order by brand_name;
+
+select order_id, SUM(quantity * list_price * (1- discount)) net_value from sales.order_items group by order_id
+
+-- having clouse
+select customer_id, YEAR(order_date), COUNT(order_id) order_count from sales.orders
+ group by customer_id ,YEAR(order_date) having COUNT(order_id) >=2 ORDER BY customer_id
+
+
+ select order_id, sum(quantity * list_price *(1- discount)) net_value 
+ from sales.order_items group by order_id
+ having sum(quantity * list_price * (1- discount)) > 2000
+ order by net_value;
+
+
+ select category_id, MAX(list_price) max_list_price, MIN(list_price) min_list_price from production.products group by category_id having MAX(list_price) > 4000 OR Min (list_price) < 500;
+
+ select category_id, avg (list_price) avg_list_price from production.products group by category_id having avg(list_price) Between 400 and 1000;
--- sqlcommands.sql
+++ sqlcommands.sql
@@ -54,3 +54,85 @@ select Distinct city, state from sales.customers;
 --Using SELECT DISTINCT with NULL
 
 select distinct phone from sales.customers order by phone;
+
+--IS NULL operator
+select first_name, last_name from sales.customers where phone = Null order by first_name, last_name;
+
+use customerDb
+
+--To test whether a value is NULL or not, you always use the IS NULL operator.
+select customer_id, first_name, last_name, phone from sales.customers where phone IS NULL order by first_name, last_name;
+
+select customer_id, first_name, last_name, phone from sales.customers where phone IS NOT NULL order by first_name, last_name;
+
+--Basic SQL Server AND operator example
+select * from production.products where category_id =1 AND list_price > 400 order by list_price DESC;
+
+select * from production.products where category_id =1 AND list_price > 400  AND brand_id =1 order by list_price DESC;
+
+-- or operator 
+select * from production.products where brand_id = 1 or brand_id = 2 AND  list_price > 1000 ORDER BY brand_id DESC;
+
+select * from production.products where (brand_id =1 or brand_id = 2) AND list_price > 1000 order by brand_id
+
+--in oerator 
+select product_name, list_price from production.products where list_price IN(89.99, 109.99, 159.99) order by list_price
+
+--between operator 
+use customerDb;
+select product_id, product_name, list_price from production.products where list_price BETWEEN 149.99 AND 199.99 ORDER BY list_price;
+
+select order_id, customer_id, order_date, order_status from sales.orders where order_date BETWEEN '2017-01-15' AND '2017-01-17' order by order_date;
+
+--like operator
+select customer_id, first_name,last_name from sales.customers where last_name LIKE 'S%' ORDER BY first_name;
+
+select customer_id, first_name,last_name from sales.customers where last_name LIKE '%er' ORDER BY first_name;
+
+select customer_id, first_name,last_name from sales.customers where last_name LIKE 't%s' ORDER BY first_name;
+
+select customer_id, first_name,last_name from sales.customers where last_name LIKE '_er%' ORDER BY first_name;
+
+select customer_id, first_name,last_name from sales.customers where last_name LIKE '[YZ]%' ORDER BY first_name;
+
+select customer_id, first_name,last_name from sales.customers where last_name LIKE '[A-C]%' ORDER BY first_name;
+
+select customer_id, first_name,last_name from sales.customers where last_name LIKE '[^A-X]%' ORDER BY first_name;
+
+select customer_id, first_name,last_name from sales.customers where last_name NOT LIKE 'A%' ORDER BY first_name;
+
+--ESCAPE WITH LIKE OPERATOR
+CREATE TABLE sales.feedbacks (
+  feedback_id INT IDENTITY(1, 1) PRIMARY KEY, 
+  comment VARCHAR(255) NOT NULL
+);
+INSERT INTO sales.feedbacks(comment)
+VALUES('Can you give me 30% discount?'),
+      ('May I get me 30USD off?'),
+      ('Is this having 20% discount today?');
+
+	  SELECT * FROM sales.feedbacks;
+
+	  select feedback_id, comment from sales.feedbacks where comment Like '%30%'
+
+	  --In this query, the  ESCAPE clause specified that the character ! is the escape character.
+	  select feedback_id, comment from sales.feedbacks where comment Like '%30!%%' ESCAPE '!';
+
+	  --ALIAS
+
+	  select first_name + ' ' + last_name AS 'FULL Name' from sales.customers order by first_name;
+
+	  select category_name 'Product Category' from production.categories order by category_name;
+
+	  -- when both table contains same fields then we have to specify name like below
+
+	  select sales.customers.customer_id , first_name, last_name, order_id from sales.customers INNER JOIN sales.orders ON sales.orders.customer_id = sales.customers.customer_id;
+
+	  select c.customer_id, first_name, last_name, order_id from sales.customers c INNER JOIN sales.orders o on o.customer_id = c.customer_id;
+
+
+
+	  
+
+
+
